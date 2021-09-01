0f809b052e7029e2da5bea994aaf8a2ebefd2dc0
Xuewen.Lei <Xuewen.Lei@ff.com>
2021-09-01 14:20:03 +0800

    0203

--- Leetcode_Python/Linkedlist/0203_Remove_Linked_List_Elements.py
+++ Leetcode_Python/Linkedlist/0203_Remove_Linked_List_Elements.py
@@ -4,3 +4,30 @@
 # @Author: Xuewen Lei
 # @Date  : 2021/9/1
 # @Desc  :
+
+
+# Definition for singly-linked list.
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
+        dummy = ListNode()
+        dummy.next = head
+        prev = dummy
+
+        while head is not None:
+            if head.val == val:
+                prev.next = head.next
+                head = head.next
+            else:
+                prev = head
+                head = head.next
+        return dummy.next
+
